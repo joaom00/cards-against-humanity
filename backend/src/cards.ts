5d867fa64eff7c61e46d5ac219b3b754c0eb16ad
joaom <joaomljcanal@gmail.com>
2021-03-12 12:06:34 -0300

    refactor cards

--- backend/src/cards.ts
+++ backend/src/cards.ts
@@ -1,8 +1,10 @@
 import whiteCards from '../cards/white';
+import blackCards from '../cards/black';
 
 interface Deck {
   room: string;
-  deck: string[];
+  whiteDeck: string[];
+  blackDeck: string[];
 }
 
 const decks = [] as Deck[];
@@ -10,7 +12,8 @@ const decks = [] as Deck[];
 export const createDeckOfRoom = (room: string) => {
   const deck = {
     room,
-    deck: [...whiteCards],
+    whiteDeck: [...whiteCards],
+    blackDeck: [...blackCards],
   };
 
   decks.push(deck);
@@ -21,20 +24,43 @@ export const createDeckOfRoom = (room: string) => {
 export const deckAlreadyExists = (room: string) =>
   decks.some((deck) => deck.room === room);
 
-export const pickFourCards = (room: string) => {
+export const pickFourWhiteCards = (room: string) => {
   const roomDeck = decks.find((deck) => deck.room === room);
   const fourCards = [];
 
   if (roomDeck) {
     for (let i = 0; i < 4; i++) {
-      const randomIndex = Math.floor(Math.random() * roomDeck.deck.length - 1);
-      const card = roomDeck.deck.splice(randomIndex, 1)[0];
+      const randomIndex = Math.floor(
+        Math.random() * roomDeck.whiteDeck.length - 1
+      );
+      const card = roomDeck.whiteDeck.splice(randomIndex, 1)[0];
       fourCards.push(card);
     }
 
-    return {
-      fourCards,
-      roomDeck,
-    };
+    return fourCards;
+  }
+};
+
+export const pickOneWhiteCard = (room: string) => {
+  const roomDeck = decks.find((deck) => deck.room === room);
+
+  if (roomDeck) {
+    const randomIndex = Math.floor(Math.random() * roomDeck.whiteDeck.length);
+
+    const whiteCard = roomDeck.whiteDeck.splice(randomIndex, 1)[0];
+
+    return whiteCard;
+  }
+};
+
+export const pickOneBlackCard = (room: string) => {
+  const roomDeck = decks.find((deck) => deck.room === room);
+
+  if (roomDeck) {
+    const randomIndex = Math.floor(Math.random() * roomDeck.blackDeck.length);
+
+    const blackCard = roomDeck.blackDeck.splice(randomIndex, 1)[0];
+
+    return blackCard;
   }
 };
