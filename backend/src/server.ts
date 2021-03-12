213dee202f0996f1920002a71feb2bacdec3cefe
joaom <joaomljcanal@gmail.com>
2021-03-12 12:06:48 -0300

    refactor server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import { addUser, getUsersInRoom } from './users';
 
-import whiteCards from '../cards/white';
-import blackCards from '../cards/black';
-import { createDeckOfRoom, deckAlreadyExists, pickFourCards } from './cards';
+import { addUser, getUsersInRoom, removeUser } from './users';
+import {
+  createDeckOfRoom,
+  deckAlreadyExists,
+  pickFourWhiteCards,
+  pickOneWhiteCard,
+  pickOneBlackCard,
+} from './cards';
 
 const app = express();
 const server = createServer(app);
@@ -19,8 +23,6 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  let blackCardsClone = [...blackCards];
-
   socket.on('joinRoom', ({ name, room }) => {
     addUser(socket.id, name, room);
 
@@ -28,16 +30,14 @@ io.on('connection', (socket) => {
 
     if (deckAlreadyExists(room)) return;
 
-    const decks = createDeckOfRoom(room);
+    createDeckOfRoom(room);
   });
 
   socket.on('getFourWhiteCards', (room) => {
     if (!room) return;
-    const fourCards = pickFourCards(room);
+    const fourCards = pickFourWhiteCards(room);
 
-    console.log(fourCards?.roomDeck.deck.length);
-
-    socket.emit('fourWhiteCards', fourCards?.fourCards);
+    socket.emit('fourWhiteCards', fourCards);
   });
 
   socket.on('getUsersInRoom', (room) => {
@@ -46,16 +46,16 @@ io.on('connection', (socket) => {
     io.to(room).emit('usersInRoom', users);
   });
 
-  // socket.on('newWhiteCard', () => {
-  //   const randomIndex = Math.floor(Math.random() * whiteCardsClone.length - 1);
-  //   const card = whiteCardsClone.splice(randomIndex, 1);
-  //   socket.emit('whiteCard', card);
-  // });
+  socket.on('newWhiteCard', (room) => {
+    const whiteCard = pickOneWhiteCard(room);
+
+    socket.emit('whiteCard', whiteCard);
+  });
 
   socket.on('newBlackCard', (room) => {
-    const randomIndex = Math.floor(Math.random() * blackCardsClone.length - 1);
-    const card = blackCardsClone.splice(randomIndex, 1);
-    io.to(room).emit('blackCard', card);
+    const blackCard = pickOneBlackCard(room);
+
+    io.to(room).emit('blackCard', blackCard);
   });
 
   socket.on('newWhiteCardSelected', (data) => {
@@ -63,11 +63,21 @@ io.on('connection', (socket) => {
     io.to(data.room).emit('whiteCardSelected', card);
   });
 
+  socket.on('setNewRound', (room) => {
+    io.to(room).emit('newRound');
+  });
+
   socket.on('disconnect', () => {
-    console.log('user had left!!!');
+    const room = removeUser(socket.id);
+
+    if (room) {
+      const users = getUsersInRoom(room);
+
+      io.to(room).emit('usersInRoom', users);
+    }
   });
 });
 
-server.listen(3333, () => {
+server.listen(process.env.PORT || 3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
