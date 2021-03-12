967829b68ee9aa4caa3610634e7b0fd689bea373
joaom <joaomljcanal@gmail.com>
2021-03-12 12:07:33 -0300

    refactor black card styles

--- frontend/src/components/BlackCard/styles.ts
+++ frontend/src/components/BlackCard/styles.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   background-color: #000;
   color: #fff;
-  width: 129px;
-  height: 158px;
+  width: 142px;
+  height: 174px;
   border-radius: 10px;
   padding: 16px;
   box-shadow: 0px 20px 40px rgba(32, 32, 32, 0.08);
@@ -12,6 +12,8 @@ export const Container = styled.div`
   flex-direction: column;
   cursor: pointer;
   overflow-y: auto;
+
+  font-family: Nunito;
 
   p {
     flex: 1;
